89f8def6beaf73756f8430c552b4381d55de999c
iLLu51v3 <jeremy_calcium@hotmail.com>
2024-06-12 18:59:51 -0500

    Retry---code 2.0

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,59 +1,93 @@
 // ❗ Create state to hold the data from the API
 // ❗ Create effects to fetch the data and put it in state
 /* ❗ Map over the data in state, rendering a Character at each iteration */
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import Character from './Character';
 
-const urlPlanets = 'http://localhost:9009/api/planets';
-const urlPeople = 'http://localhost:9009/api/people';
+import React from 'react'
+import axios from 'axios'
+import Character from './Character'
 
-function App() {
-  // const [planet, setPlanet] = useState();
-  const [character, setCharacter] = useState();
-  
-  useEffect(() => {
-    Promise.all([axios.get(urlPlanets), axios.get(urlPeople)])
-      .then(([planetResponse, characterResponse]) => {
-        const planetData = planetResponse.data;
-        const characterData = characterResponse.data;
+const urlPlanets = 'http://localhost:9009/api/planets'
+const urlPeople = 'http://localhost:9009/api/people'
 
-        const updatedCharacter = characterData.map(character => {
-          const homeworld = planetData.find(planet => planet.id === character.homeworld);
-          return { ...character, homeworld };
-        });
-      
-        setCharacter(updatedCharacter);
-      })
-      .catch(error => {
-          console.log(error);
-        });
-    }, []);
-
+function App() { 
+  // ❗ Create state to hold the data from the API
+  // ❗ Create effects to fetch the data and put it in state
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      {character && character.map(character => (
-        <Character
-        key={character.id}
-        name={character.name}
-        birthYear={character.birth_year}
-        created={character.created}
-        edited={character.edited}
-        eyeColor={character.eye_color}
-        gender={character.gender}
-        hairColor={character.hair_color}
-        height={character.height}
-        mass={character.mass}
-        skinColor={character.skin_color}
-        homeworld={character.homeworld}
-        />
-      ))}
+      <p>See the README of the project for instructions on completing this challenge</p>
+      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
+
+
+
+
+
+
+
+// ______________________________________________________________//
+
+
+// import React, {useState, useEffect} from 'react';
+// import axios from 'axios';
+// import Character from './Character';
+
+// const urlPlanets = 'http://localhost:9009/api/planets';
+// const urlPeople = 'http://localhost:9009/api/people';
+
+// function App() {
+//   // const [planet, setPlanet] = useState();
+//   const [character, setCharacter] = useState();
+  
+//   useEffect(() => {
+//     Promise.all([axios.get(urlPlanets), axios.get(urlPeople)])
+//       .then(([planetResponse, characterResponse]) => {
+//         const planetData = planetResponse.data;
+//         const characterData = characterResponse.data;
+
+//         const updatedCharacter = characterData.map(character => {
+//           const homeworld = planetData.find(planet => planet.id === character.homeworld);
+//           return { ...character, homeworld };
+//         });
+      
+//         setCharacter(updatedCharacter);
+//       })
+//       .catch(error => {
+//           console.log(error);
+//         });
+//     }, []);
+
+//   return (
+//     <div>
+//       <h2>Star Wars Characters</h2>
+//       {character && character.map(character => (
+//         <Character
+//         key={character.id}
+//         name={character.name}
+//         birthYear={character.birth_year}
+//         created={character.created}
+//         edited={character.edited}
+//         eyeColor={character.eye_color}
+//         gender={character.gender}
+//         hairColor={character.hair_color}
+//         height={character.height}
+//         mass={character.mass}
+//         skinColor={character.skin_color}
+//         homeworld={character.homeworld}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// // ❗ DO NOT CHANGE THE CODE  BELOW
+// if (typeof module !== 'undefined' && module.exports) module.exports = App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -2,45 +2,70 @@
 // ❗ Create a "toggle" click handler to show or remove the homeworld
 // ❗ Add the props
 // I tried to set "Details:" to show only after the character card onclick was performed to then have character details be presented, but from what I could figure, doing so whould require changing the character-card's onclick code, thus losing out on holding 100% completion; Other possibilities would require ummmm... a bunch of conditional if statments and/or click count conditional parameters. 
-import React, {useState} from 'react'
 
-function Character(props) { 
-  const [showHomeworld, setShowHomeworld] = useState(false);
-  const [showCharacterDetails, setShowCharacterDetails] = useState(false);
-  const toggleHomeworld = () => {
-    setShowHomeworld(!showHomeworld);
-  };
-  const toggleCharacterDetails = () => {
-    setShowCharacterDetails(!showCharacterDetails);
-  };
-  const handleCardClick = () => {
-    toggleHomeworld();
-    toggleCharacterDetails();
-  };
+import React from 'react'
+
+function Character() { // ❗ Add the props
+  // ❗ Create a state to hold whether the homeworld is rendering or not
+  // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div className="character-card" onClick={handleCardClick}>
+    <div>
       {/* Use the same markup with the same attributes as in the mock */}
-      <h3 className="character-name">{props.name}</h3>
-      <h4 className="character-details" onClick={toggleCharacterDetails}> Details:</h4>
-      {showCharacterDetails && (
-        <>
-          <p>Birth Year: {props.birthYear}</p>
-          <p>Created: {props.created}</p>
-          <p>Edited: {props.edited}</p>
-          <p>Eye Color: {props.eyeColor}</p>
-          <p>Gender: {props.gender}</p>
-          <p>Hair Color: {props.hairColor}</p>
-          <p>Height: {props.height}</p>
-          <p>Mass: {props.mass}</p>
-          <p>Skin Color: {props.skinColor}</p>
-        </>
-      )}
-      {showHomeworld && (
-        <p className="character-planet">Planet:{props.homeworld.name}</p>
-      )}
-      
     </div>
-  );
+  )
 }
 
-export default Character;
+export default Character
+
+
+
+
+
+
+
+// ______________________________________________________________//
+
+
+// import React, {useState} from 'react'
+
+// function Character(props) { 
+//   const [showHomeworld, setShowHomeworld] = useState(false);
+//   const [showCharacterDetails, setShowCharacterDetails] = useState(false);
+//   const toggleHomeworld = () => {
+//     setShowHomeworld(!showHomeworld);
+//   };
+
+//   const toggleCharacterDetails = () => {
+//     setShowCharacterDetails(!showCharacterDetails);
+//   };
+//   const handleCardClick = () => {
+//     toggleHomeworld();
+//     toggleCharacterDetails();
+//   };
+//   return (
+//     <div className="character-card" onClick={handleCardClick}>
+//       {/* Use the same markup with the same attributes as in the mock */}
+//       <h3 className="character-name">{props.name}</h3>
+//       <h4 className="character-details" onClick={toggleCharacterDetails}> Details:</h4>
+//       {showCharacterDetails && (
+//         <>
+//           <p>Birth Year: {props.birthYear}</p>
+//           <p>Created: {props.created}</p>
+//           <p>Edited: {props.edited}</p>
+//           <p>Eye Color: {props.eyeColor}</p>
+//           <p>Gender: {props.gender}</p>
+//           <p>Hair Color: {props.hairColor}</p>
+//           <p>Height: {props.height}</p>
+//           <p>Mass: {props.mass}</p>
+//           <p>Skin Color: {props.skinColor}</p>
+//         </>
+//       )}
+//       {showHomeworld && (
+//         <p className="character-planet">Planet:{props.homeworld.name}</p>
+//       )}
+
+//     </div>
+//   );
+// }
+
+// export default Character;
